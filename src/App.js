f6e60c55ab819b52bedb37b9e012f3326c370f35
Luckyrana001 <Luckyrana001@gmail.com>
2024-03-29 00:06:58 +0530

    created hashed password

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
+import { createHash } from 'crypto';
+
 
 function App() {
 
@@ -13,10 +15,38 @@ function App() {
 
   const keyBase64 = "U+M3HkGeOvIs2JVlJso1zw==";
   const rawIv = "AAAAAAAAAAAAAAAAAAAAAA==";
-
+ 
+  function getHashedPassword(passwordToHash, salt) {
+    let generatedPassword = null;
+    try {
+      const md5Hash = createHash('md5');
+      md5Hash.update(salt);
+      md5Hash.update(passwordToHash);
+      const hashResult = md5Hash.digest();
+      const base64Encoded = Buffer.from(hashResult).toString('base64');
+      generatedPassword = base64Encoded.replace(/\n/g, ''); // Remove newline characters
+      console.log("generatedPassword==========="+generatedPassword)
+    } catch (error) {
+      console.error('Hashing error:', error);
+    }
+    return generatedPassword;
+  }
 
   useEffect(() => {
+
+    const userName = 'ymcauser';
+    const password = 'password';
+    const requestId = '88888888';
+
+
+   const hashedPassword = getHashedPassword(password,requestId)
+    const newString = userName+':'+hashedPassword;
+    const encodedString = btoa(newString);
+    const  finalString = encodedString.replace(/\n/g, ''); // Remove newline characters
+     console.log("Encoded string:", finalString);
+
     InitializeEncryption();
+
   }, []);
 
   function InitializeEncryption() {
